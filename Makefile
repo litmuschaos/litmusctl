2be4eca9c5934f1484fd1c7cfc5b99dd5ef1a756
Raj Babu Das <mail.rajdas@gmail.com>
2021-03-14 15:29:45 +0530

    Adding new build script for github workflow (#4)
    * Adding CI/CD for litmusctl and multiplatform support
    
    Signed-off-by: rajdas98 <mail.rajdas@gmail.com>

--- Makefile
+++ Makefile
@@ -3,8 +3,16 @@ PKG := "github.com/litmuschaos/$(PROJECT_NAME)"
 
 all: build
 
-dep: ## Get the dependencies
-	@go mod download
+build: ## Build the binary file
+	@bash scripts/build.sh main.go
 
-build: dep ## Build the binary file
-	@go build -i -o build/main $(PKG)
+
+.PHONY: unused-package-check
+unused-package-check:
+	@echo "------------------"
+	@echo "--> Check unused packages for the litmusctl"
+	@echo "------------------"
+	@tidy=$$(go mod tidy); \
+	if [ -n "$${tidy}" ]; then \
+		echo "go mod tidy checking failed!"; echo "$${tidy}"; echo; \
+	fi
---
+++ scripts/build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+package=$1
+if [[ -z "$package" ]]; then
+  echo "usage: $0 <package-name>"
+  exit 1
+fi
+
+platforms=(
+"linux/amd64"
+"linux/arm"
+"linux/arm64"
+)
+
+for platform in "${platforms[@]}"
+do
+    platform_split=(${platform//\// })
+    GOOS=${platform_split[0]}
+    GOARCH=${platform_split[1]}
+    echo 'Building' $GOOS-$GOARCH
+    output_name='litmusctl-'$GOOS'-'$GOARCH
+
+    env GOOS=$GOOS GOARCH=$GOARCH go build -v -o platforms/$output_name $package
+    if [ $? -ne 0 ]; then
+        echo 'An error has occurred! Aborting the script execution...'
+        exit 1
+    fi
+done
