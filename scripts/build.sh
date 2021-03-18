62bce9a32802881eea65a0c87317125bb01991a1
arkajyotiMukherjee <arkajyoti.mukherjee@mayadata.io>
2021-03-18 20:04:35 +0530

    added more platforms types for litmusctl
    Signed-off-by: arkajyotiMukherjee <arkajyoti.mukherjee@mayadata.io>

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,9 +7,26 @@ if [[ -z "$package" ]]; then
 fi
 
 platforms=(
+"darwin/386"
+"dragonfly/amd64"
+"freebsd/386"
+"freebsd/amd64"
+"freebsd/arm"
+"linux/386"
 "linux/amd64"
 "linux/arm"
 "linux/arm64"
+"netbsd/386"
+"netbsd/amd64"
+"netbsd/arm"
+"openbsd/386"
+"openbsd/amd64"
+"openbsd/arm"
+"plan9/386"
+"plan9/amd64"
+"solaris/amd64"
+"windows/amd64"
+"windows/386"
 )
 
 for platform in "${platforms[@]}"
