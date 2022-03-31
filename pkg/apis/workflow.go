d5e63661062de16db44a1ced8525543a44dd25d9
PrayagS <prayagsavsani@gmail.com>
2022-03-31 10:01:13 +0530

    feat(workflows): Init create workflow command
    Signed-off-by: PrayagS <prayagsavsani@gmail.com>

---
+++ pkg/apis/workflow.go
@@ -0,0 +1,56 @@
+/*
+Copyright © 2021 The LitmusChaos Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package apis
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	types "github.com/litmuschaos/litmusctl/pkg/types"
+	"github.com/litmuschaos/litmusctl/pkg/utils"
+)
+
+func CreateWorkflow(in types.ChaosWorkFlowInput, cred types.Credentials) {
+
+	var gqlReq types.ChaosWorkFlowGraphQLRequest
+
+	gqlReq.Query = `mutation createChaosWorkFlow($ChaosWorkFlowInput: ChaosWorkFlowInput!) {
+                      createChaosWorkFlow(input: $ChaosWorkFlowInput) {
+                        workflow_id
+                        cronSyntax
+                        workflow_name
+                        workflow_description
+                        isCustomWorkflow
+                      }
+                    }`
+	gqlReq.Variables.ChaosWorkFlowInput = in
+
+	query, _ := json.Marshal(gqlReq)
+
+	resp, _ := SendRequest(
+		SendRequestParams{
+			Endpoint: cred.Endpoint + utils.GQLAPIPath,
+			Token:    cred.Token,
+		},
+		query,
+		string(types.Post),
+	)
+
+	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	fmt.Println(string(bodyBytes))
+}
---
+++ pkg/cmd/create/workflow.go
@@ -0,0 +1,102 @@
+/*
+Copyright © 2021 The LitmusChaos Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package create
+
+import (
+	"encoding/json"
+	"io/ioutil"
+
+	"github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
+	chaosTypes "github.com/litmuschaos/chaos-operator/pkg/apis/litmuschaos/v1alpha1"
+	"github.com/litmuschaos/litmusctl/pkg/apis"
+	"github.com/litmuschaos/litmusctl/pkg/utils"
+	"sigs.k8s.io/yaml"
+
+	types "github.com/litmuschaos/litmusctl/pkg/types"
+
+	"github.com/spf13/cobra"
+)
+
+// workflowCmd represents the project command
+var workflowCmd = &cobra.Command{
+	Use: "workflow",
+	Short: `Create a workflow
+	Example:
+	#create a workflow
+	litmusctl create workflow -f workflow.yaml
+
+	Note: The default location of the config file is $HOME/.litmusconfig, and can be overridden by a --config flag
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		credentials, err := utils.GetCredentials(cmd)
+		utils.PrintError(err)
+
+		workflowManifest, err := cmd.Flags().GetString("file")
+		utils.PrintError(err)
+
+		var chaosWorkFlowInput types.ChaosWorkFlowInput
+		var chaosExperiment chaosTypes.ChaosExperiment
+		var weightages []types.WeightagesInput
+
+		workflow := readManifestFile(workflowManifest)
+		workflowStr, _ := json.Marshal(workflow)
+
+		for _, t := range workflow.Spec.Templates {
+			if len(t.Inputs.Artifacts) != 0 {
+				err := yaml.Unmarshal([]byte(t.Inputs.Artifacts[0].Raw.Data), &chaosExperiment)
+				if err == nil {
+					continue // Tried to parse a ChaosEngine spec
+				}
+			}
+		}
+
+		weightages = append(weightages,
+			types.WeightagesInput{
+				ExperimentName: chaosExperiment.ObjectMeta.Name,
+				Weightage:      10, // TODO: fetch from annotation
+			},
+		)
+
+		chaosWorkFlowInput = types.ChaosWorkFlowInput{
+			WorkflowID:          "",
+			WorkflowManifest:    string(workflowStr),
+			CronSyntax:          "",
+			WorkflowName:        workflow.ObjectMeta.Name,
+			WorkflowDescription: "Testing struct",
+			Weightages:          weightages,
+			IsCustomWorkflow:    true,
+			ProjectID:           "c370c344-ab5d-4518-9b9c-1379eb1befcd",
+			ClusterID:           "1c9c5801-8789-4ac9-bf5f-32649b707a5c",
+		}
+
+		apis.CreateWorkflow(chaosWorkFlowInput, credentials)
+	},
+}
+
+// TODO: Move this to utils
+func readManifestFile(file string) v1alpha1.Workflow {
+	var body []byte
+	var workflowManifest v1alpha1.Workflow
+
+	body, _ = ioutil.ReadFile(file)
+	_ = yaml.Unmarshal(body, &workflowManifest)
+	return workflowManifest
+}
+
+func init() {
+	CreateCmd.AddCommand(workflowCmd)
+	workflowCmd.Flags().StringP("file", "f", "", "The manifest file for the workflow")
+}
