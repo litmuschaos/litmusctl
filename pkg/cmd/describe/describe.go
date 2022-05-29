cb26b01b1cb96f22feb301dff5055ecfb9e04790
PrayagS <prayagsavsani@gmail.com>
2022-05-29 20:44:47 +0530

    feat(workflows): Add `describe workflow` command
    Signed-off-by: PrayagS <prayagsavsani@gmail.com>

---
+++ pkg/cmd/describe/describe.go
@@ -0,0 +1,32 @@
+/*
+Copyright © 2021 The LitmusChaos Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package describe
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// DescribeCmd represents the describe command
+var DescribeCmd = &cobra.Command{
+	Use: "describe",
+	Short: `Describe resources for LitmusChaos agent plane.
+		Examples:
+		#describe a ChaosWorkflow
+		litmusctl describe workflow d861b650-1549-4574-b2ba-ab754058dd04 --project-id="d861b650-1549-4574-b2ba-ab754058dd04"
+
+		Note: The default location of the config file is $HOME/.litmusconfig, and can be overridden by a --config flag
+	`,
+}
--- pkg/cmd/root/root.go
+++ pkg/cmd/root/root.go
@@ -26,6 +26,7 @@ import (
 	"github.com/litmuschaos/litmusctl/pkg/cmd/connect"
 	"github.com/litmuschaos/litmusctl/pkg/cmd/disconnect"
 	"github.com/litmuschaos/litmusctl/pkg/cmd/delete"
+	"github.com/litmuschaos/litmusctl/pkg/cmd/describe"
 	"github.com/litmuschaos/litmusctl/pkg/cmd/upgrade"
 	"github.com/litmuschaos/litmusctl/pkg/cmd/version"
 	"github.com/litmuschaos/litmusctl/pkg/utils"
@@ -64,6 +65,7 @@ func init() {
 	rootCmd.AddCommand(connect.ConnectCmd)
 	rootCmd.AddCommand(disconnect.DisconnectCmd)
 	rootCmd.AddCommand(delete.DeleteCmd)
+	rootCmd.AddCommand(describe.DescribeCmd)
 	rootCmd.AddCommand(version.VersionCmd)
 	rootCmd.AddCommand(upgrade.UpgradeCmd)
 
