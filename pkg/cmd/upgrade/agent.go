7ac7816f7d7dd383f1cd3adcf74ca6ff8fb8252a
nagesh bansal <nageshbansal59@gmail.com>
2023-08-14 17:14:48 +0530

    Fix gofmt
    Signed-off-by: nagesh bansal <nageshbansal59@gmail.com>

--- pkg/cmd/upgrade/agent.go
+++ pkg/cmd/upgrade/agent.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -52,7 +52,7 @@ var agentCmd = &cobra.Command{
 		kubeconfig, err := cmd.Flags().GetString("kubeconfig")
 		utils.PrintError(err)
 
-		output, err := apis.UpgradeAgent(context.Background(), credentials, projectID, cluster_id, kubeconfig)
+		output, err := apis.UpgradeInfra(context.Background(), credentials, projectID, cluster_id, kubeconfig)
 		if err != nil {
 			utils.Red.Print("\n❌ Failed upgrading Chaos Delegate: \n" + err.Error() + "\n")
 			os.Exit(1)
