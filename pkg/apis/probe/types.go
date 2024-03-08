8f589e6d2a1a260d04faf164a5ba4f5fe5849d73
Shivam Purohit <shivampurohit900@gmail.com>
2024-03-08 12:16:43 +0530

    add probe-id flag for probe details
    Signed-off-by: Shivam Purohit <shivampurohit900@gmail.com>

--- pkg/apis/probe/types.go
+++ pkg/apis/probe/types.go
@@ -2,6 +2,26 @@ package probe
 
 import model "github.com/litmuschaos/litmus/chaoscenter/graphql/server/graph/model"
 
+type GetProbeGQLRequest struct {
+	Query     string `json:"query"`
+	Variables struct {
+		ProjectID string `json:"projectID"`
+		ProbeName string `json:"probeName"`
+	} `json:"variables"`
+}
+
+type GetProbeResponse struct {
+	Errors []struct {
+		Message string   `json:"message"`
+		Path    []string `json:"path"`
+	} `json:"errors"`
+	Data GetProbeResponseData `json:"data"`
+}
+
+type GetProbeResponseData struct {
+	GetProbe model.Probe `json:"getProbe"`
+}
+
 type ListProbeGQLRequest struct {
 	Query     string `json:"query"`
 	Variables struct {
--- pkg/cmd/get/probe.go
+++ pkg/cmd/get/probe.go
@@ -50,114 +50,141 @@ var probesCmd = &cobra.Command{
 				os.Exit(1)
 			}
 		}
-
-		NoninteractiveMode, err := cmd.Flags().GetBool("non-interactive")
-
-		var selectedItems []*models.ProbeType
-
-		if NoninteractiveMode == false {
-			prompt := promptui.Select{
-				Label: "Do you want to enable advance filter probes?",
-				Items: []string{"Yes", "No"},
-			}
-			_, option, err := prompt.Run()
-			if err != nil {
-				fmt.Printf("Prompt failed %v\n", err)
-				return
-			}
-			fmt.Printf("You chose %q\n", option)
-
-			if option == "Yes" {
-				items := []models.ProbeType{"httpProbe", "cmdProbe", "promProbe", "k8sProbe", "done"}
-				for {
+		ProbeID, err := cmd.Flags().GetString("probe-id")
+		
+		if ProbeID == "" {
+			//check for interactive or not interactive mode
+			//also give option to the user for getting probe details of a single probe
+			
+			var selectedItems []*models.ProbeType
+			NoninteractiveMode,err := cmd.Flags().GetBool("non-interactive")
+			utils.PrintError(err)
+			
+			if NoninteractiveMode == false {
+				prompt := promptui.Select{
+					Label: "Do you want details about a single Probe?",
+					Items: []string{"Yes", "No"},
+				}
+				_, option, err := prompt.Run()
+				if err != nil {
+					fmt.Printf("Prompt failed %v\n", err)
+					return
+				}
+				fmt.Printf("You chose %q\n", option)
+				if option == "Yes" {
+					utils.Red.Println("calling that same get probe thing")
+	
+				} else {
 					prompt := promptui.Select{
-						Label: "Select ProbeType",
-						Items: items,
-						Templates: &promptui.SelectTemplates{
-							Active:   `▸ {{ . | cyan }}`,
-							Inactive: `  {{ . | white }}`,
-							Selected: `{{ "✔" | green }} {{ . | bold }}`,
-						},
+						Label: "Do you want to enable advance filter probes?",
+						Items: []string{"Yes", "No"},
 					}
-
-					selectedIndex, result, err := prompt.Run()
+					_, option, err := prompt.Run()
 					if err != nil {
 						fmt.Printf("Prompt failed %v\n", err)
-						os.Exit(1)
+						return
 					}
-
-					if items[selectedIndex] == "done" {
-						break
+					fmt.Printf("You chose %q\n", option)
+		
+					if option == "Yes" {
+						items := []models.ProbeType{"httpProbe", "cmdProbe", "promProbe", "k8sProbe", "done"}
+						for {
+							prompt := promptui.Select{
+								Label: "Select ProbeType",
+								Items: items,
+								Templates: &promptui.SelectTemplates{
+									Active:   `▸ {{ . | cyan }}`,
+									Inactive: `  {{ . | white }}`,
+									Selected: `{{ "✔" | green }} {{ . | bold }}`,
+								},
+							}
+		
+							selectedIndex, result, err := prompt.Run()
+							if err != nil {
+								fmt.Printf("Prompt failed %v\n", err)
+								os.Exit(1)
+							}
+		
+							if items[selectedIndex] == "done" {
+								break
+							}
+		
+							final := models.ProbeType(result)
+							selectedItems = append(selectedItems, &final)
+							items = append(items[:selectedIndex], items[selectedIndex+1:]...)
+		
+						}
+		
+						fmt.Printf("Selected Probe Types: %v\n", selectedItems)
 					}
-
-					final := models.ProbeType(result)
-					selectedItems = append(selectedItems, &final)
-					items = append(items[:selectedIndex], items[selectedIndex+1:]...)
-
 				}
-
-				fmt.Printf("Selected Probe Types: %v\n", selectedItems)
+	
+			} else {
+				var probeTypes string
+				probeTypes, err = cmd.Flags().GetString("probe-types")
+				values := strings.Split(probeTypes, ",")
+				for _, value := range values {
+					probeType := models.ProbeType(value)
+					selectedItems = append(selectedItems, &probeType)
+				}
 			}
-		} else {
-			var probeTypes string
-			probeTypes, err = cmd.Flags().GetString("probe-types")
-			values := strings.Split(probeTypes, ",")
-			for _, value := range values {
-				probeType := models.ProbeType(value)
-				selectedItems = append(selectedItems, &probeType)
-			}
-		}
-
-		probes_get, _ := apis.ListProbeRequest(projectID, selectedItems, credentials)
-		probes_data := probes_get.Data.Probes
-
-		itemsPerPage := 5
-		page := 1
-		totalProbes := len(probes_data)
-
-		writer := tabwriter.NewWriter(os.Stdout, 8, 8, 8, '\t', tabwriter.AlignRight)
-		utils.White_B.Fprintln(writer, "PROBE ID\t PROBE TYPE\t CREATED AT\t CREATED BY")
-
-		for {
-			writer.Flush()
-			// calculating the start and end indices for the current page
-			start := (page - 1) * itemsPerPage
-			if start >= totalProbes {
-				utils.Red.Println("No more probes to display")
+	
+			probes_get, _ := apis.ListProbeRequest(projectID, selectedItems, credentials)
+			probes_data := probes_get.Data.Probes
+	
+			itemsPerPage := 5
+			page := 1
+			totalProbes := len(probes_data)
+	
+			writer := tabwriter.NewWriter(os.Stdout, 8, 8, 8, '\t', tabwriter.AlignRight)
+			utils.White_B.Fprintln(writer, "PROBE ID\t PROBE TYPE\t CREATED AT\t CREATED BY")
+	
+			for {
 				writer.Flush()
-				break
-			}
-			end := start + itemsPerPage
-			if end > totalProbes {
-				end = totalProbes
-			}
-			for _, probe := range probes_data[start:end] {
-				intTime, err := strconv.ParseInt(probe.CreatedAt, 10, 64)
-				if err != nil {
-					fmt.Println("Error converting CreatedAt to int64:", err)
-					continue
+				// calculating the start and end indices for the current page
+				start := (page - 1) * itemsPerPage
+				if start >= totalProbes {
+					utils.Red.Println("No more probes to display")
+					writer.Flush()
+					break
 				}
-				humanTime := time.Unix(intTime, 0)
-				utils.White.Fprintln(writer, probe.Name+"\t"+fmt.Sprintf("%v", probe.Type)+"\t"+probe.CreatedBy.Username+"\t"+humanTime.String())
+				end := start + itemsPerPage
+				if end > totalProbes {
+					end = totalProbes
+				}
+				for _, probe := range probes_data[start:end] {
+					intTime, err := strconv.ParseInt(probe.CreatedAt, 10, 64)
+					if err != nil {
+						fmt.Println("Error converting CreatedAt to int64:", err)
+						continue
+					}
+					humanTime := time.Unix(intTime, 0)
+					utils.White.Fprintln(writer, probe.Name+"\t"+fmt.Sprintf("%v", probe.Type)+"\t"+probe.CreatedBy.Username+"\t"+humanTime.String())
+				}
+				writer.Flush()
+	
+				// Check if it's the last item or if user wants to see more
+				paginationPrompt := promptui.Prompt{
+					Label:     "Press Enter to show more probes (or type 'q' to quit)",
+					AllowEdit: true,
+					Default:   "",
+				}
+	
+				userInput, err := paginationPrompt.Run()
+				utils.PrintError(err)
+	
+				if userInput == "q" {
+					break
+				}
+				// Move to the next page
+				page++
 			}
-			writer.Flush()
 
-			// Check if it's the last item or if user wants to see more
-			paginationPrompt := promptui.Prompt{
-				Label:     "Press Enter to show more probes (or type 'q' to quit)",
-				AllowEdit: true,
-				Default:   "",
-			}
-
-			userInput, err := paginationPrompt.Run()
-			utils.PrintError(err)
-
-			if userInput == "q" {
-				break
-			}
-			// Move to the next page
-			page++
+		} else {
+			//call the probe get endpoint to get the probes details
+			utils.Red.Println("get probe endpoint")
 		}
+
 	},
 }
 
@@ -167,4 +194,5 @@ func init() {
 	probesCmd.Flags().String("project-id", "", "Set the project-id to get Probe from a particular project.")
 	probesCmd.Flags().BoolP("non-interactive", "n", false, "Set it to true for non interactive mode | Note: Always set the boolean flag as --non-interactive=Boolean")
 	probesCmd.Flags().String("probe-types", "", "Set the probe-types as comma separated values to filter the probes")
+	probesCmd.Flags().String("probe-id", "", "Set the probe-details to the ID of probe for getting all the details related to the probe.")
 }
